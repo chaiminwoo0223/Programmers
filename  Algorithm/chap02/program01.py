409c4d9f76e7069d5fbd044467ad3c7110f7e780
최민우 <chaiminwoo0223@gmail.com>
2024-09-15 23:14:45 +0900

    Delete  Algorithm/chap02 directory

---  Algorithm/chap02/program01.py
+++
@@ -1,7 +0,0 @@
-# 같은 숫자는 싫어
-def solution(elements):
-    result = []
-    for element in elements:
-        if len(result) == 0 or result[-1] != element:
-            result.append(element)
-    return result
