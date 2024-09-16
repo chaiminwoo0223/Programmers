3c4a8012aca4ca0e564c28affa193192037a3c96
Choi, Minwoo <chaiminwoo0223@gmail.com>
2024-09-16 17:32:36 +0900

    완주하지 못한 선수

--- Algorithm/chap01/program02.py
+++ Algorithm/chap01/program02.py
@@ -1,9 +1,6 @@
 # 완주하지 못한 선수
+from collections import Counter
+
 def solution(participant, completion):
-    participant.sort()
-    completion.sort()
-    
-    for p, c in zip(participant, completion):
-        if p != c:
-            return p
-    return participant[-1]
+    result = Counter(participant) - Counter(completion)
+    return list(result)[-1]
