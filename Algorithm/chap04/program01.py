333231fe9c6e4305f6593d0b5fb120c365cbc40b
Choi, Minwoo <chaiminwoo0223@gmail.com>
2024-09-18 17:23:08 +0900

    K번째수

--- Algorithm/chap04/program01.py
+++ Algorithm/chap04/program01.py
@@ -1,12 +1,8 @@
 # K번째수
 def solution(array, commands):
     answer = []
-    for command in commands:
-        i = command[0]; j = command[1]; k = command[2]
+    for command in commands: 
+        i, j, k = command # 핵심
         answer.append(sorted(array[i-1:j])[k-1])
     
     return answer
-    
-array = [1, 5, 2, 6, 3, 7, 4]
-commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
-print(solution(array, commands))
