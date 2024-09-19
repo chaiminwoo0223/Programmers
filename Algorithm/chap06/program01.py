1a22a89ff43f602929eacfccccc5345659afed50
Choi, Minwoo <chaiminwoo0223@gmail.com>
2024-09-19 22:33:30 +0900

    체육복

--- Algorithm/chap06/program01.py
+++ Algorithm/chap06/program01.py
@@ -1,19 +1,14 @@
 # 체육복
 def solution(n, lost, reserve):
-    lost.sort()
-    reserve.sort()
+    lost = set(lost)
+    reserve = set(reserve)
+    reserved = reserve - lost # 여벌 체육복을 가지고 있지만, 도난당한 학생 제거
+    losted = lost - reserve # 여벌 체육복을 가지고 있지만, 도난당한 학생 제거
     
-    for i in lost[:]: # 얕은 복사본 순회(순회 영향 없음)
-        if i in reserve:
-            reserve.remove(i)
-            lost.remove(i)
-    for i in lost[:]: # 얕은 복사본 순회(순회 영향 없음)
-        if (i-1) in reserve:
-            reserve.remove(i-1)
-            lost.remove(i)
-        elif (i+1) in reserve:
-            reserve.remove(i+1)
-            lost.remove(i)
-            
-    answer = n - len(lost)
-    return answer
+    for r in sorted(reserved):
+        if r-1 in losted:
+            losted.remove(r-1)
+        elif r+1 in losted:
+            losted.remove(r+1)
+
+    return n - len(losted)
