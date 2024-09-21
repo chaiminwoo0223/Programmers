1888adf59e2f3e2401ac2480970224c86c08124a
Choi, Minwoo <chaiminwoo0223@gmail.com>
2024-09-22 01:16:56 +0900

    H-Index

---
+++ Algorithm/chap04/program03.py
@@ -0,0 +1,9 @@
+# H-Index
+def solution(citations):
+    citations.sort()
+    n = len(citations)
+    
+    for i in range(n):
+        if citations[i] >= (n-i): 
+            return n-i
+    return 0
