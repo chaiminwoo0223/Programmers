b47bf5f77f1f813762c94e880fe5a4e60e34c527
Choi, Minwoo <chaiminwoo0223@gmail.com>
2024-09-30 23:05:55 +0900

    모음 사전

--- Algorithm/chap05/program06.py
+++ Algorithm/chap05/program06.py
@@ -2,10 +2,6 @@
 from itertools import product
 
 def solution(word):
-    dictionary = []
-    for i in range(1, 6):
-        for j in product(['A', 'E', 'I', 'O', 'U'], repeat = i):
-            dictionary.append(''.join(j))
+    dictionary = [''.join(j) for i in range(1, 6) for j in product(['A', 'E', 'I', 'O', 'U'], repeat = i)] 
     dictionary = sorted(dictionary)
-    
     return dictionary.index(word) + 1
