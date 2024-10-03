7eaf572bb211d6c2fd305f221407ffc8d36b1b64
Choi, Minwoo <chaiminwoo0223@gmail.com>
2024-10-03 23:19:35 +0900

    JadenCase 문자열 만들기

--- Level/level2/program03.py
+++ Level/level2/program03.py
@@ -1,13 +1,15 @@
 # JadenCase 문자열 만들기
+# '': 빈 문자열 vs ' ': 공백 문자
 def solution(sentences):
-    sentences = sentences.split(" ") # 공백을 유지하면서 단어를 나눈다.
-    answer = []
+    sentences = sentences.split(' ') # 공백을 유지하면서 단어를 나눈다.(문자열 -> 리스트)
+    result = []
     
     for sentence in sentences:
-        if len(sentence) > 0 and sentence[0].isalpha(): # IndexError 방지
-            sentence = sentence[0].upper() + sentence[1:].lower()
+        if sentence == '':
+            result.append(sentence)  
+        elif sentence.isalpha():
+            result.append(sentence[0].upper() + sentence[1:].lower())
         else:
-            sentence = sentence.lower()
-        answer.append(sentence)
-        
-    return " ".join(answer)
+            result.append(sentence.lower())
+            
+    return " ".join(result)
