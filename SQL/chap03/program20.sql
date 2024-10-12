a9dbc1f49ac99cb7a34fcf1f335cf12fcd91a795
Choi, Minwoo <chaiminwoo0223@gmail.com>
2024-10-12 21:07:42 +0900

    식품분류별 가장 비싼 식품의 정보 조회하기

--- SQL/chap03/program20.sql
+++ SQL/chap03/program20.sql
@@ -2,7 +2,5 @@
 SELECT CATEGORY, PRICE AS MAX_PRICE, PRODUCT_NAME
 FROM food_product
 WHERE CATEGORY IN ("과자", "국", "김치", "식용유") AND 
-      PRICE IN (SELECT MAX(PRICE) FROM food_product 
-                WHERE CATEGORY IN ("과자", "국", "김치", "식용유") 
-                GROUP BY CATEGORY)
+      PRICE IN (SELECT MAX(PRICE) FROM food_product WHERE CATEGORY IN ("과자", "국", "김치", "식용유") GROUP BY CATEGORY)
 ORDER BY MAX_PRICE DESC;
