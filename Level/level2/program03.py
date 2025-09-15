1bd548772bb0cb0281d7c5ecaa8e2cad3f1e3f8b
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-09-15 18:32:57 +0900

    JadenCase 문자열

--- Level/level2/program03.py
+++ Level/level2/program03.py
@@ -1,15 +1,16 @@
-# JadenCase 문자열 만들기
-# '': 빈 문자열 vs ' ': 공백 문자
-def solution(sentences):
-    sentences = sentences.split(' ') # 공백을 유지하면서 단어를 나눈다.(문자열 -> 리스트)
-    result = []
-    
-    for sentence in sentences:
-        if sentence == '':
-            result.append(sentence)  
-        elif sentence.isalpha():
-            result.append(sentence[0].upper() + sentence[1:].lower())
+# JadenCase 문자열
+# 다중 공백 처리
+def solution(s):
+    result = ""
+
+    for i, word in enumerate(s):
+        if word in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"):
+            result += word
+        elif word == " ":
+            result += " "
+        elif len(result) == 0 or result[-1] == " ":
+            result += word.upper()
         else:
-            result.append(sentence.lower())
-            
-    return " ".join(result)
+            result += word.lower()
+
+    return result
