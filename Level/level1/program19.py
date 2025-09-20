a7c0a2f59d0f35a5f80a23374a9f175a22f7f324
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-09-20 18:58:02 +0900

    콜라츠 추측

---
+++ Level/level1/program19.py
@@ -0,0 +1,15 @@
+# 콜라츠 추측
+def solution(num):
+    if num == 1:
+        return 0
+
+    for i in range(500):
+        if num % 2 == 0:
+            num //= 2
+        else:
+            num = (num * 3) + 1
+
+        if num == 1:
+            return i + 1
+
+    return -1
