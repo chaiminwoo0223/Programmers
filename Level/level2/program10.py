af436c91a2b8065ce6c8ba482ec289561fc5600e
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-09-23 18:18:57 +0900

    짝지어 제거하기

--- Level/level2/program10.py
+++ Level/level2/program10.py
@@ -1,18 +1,15 @@
 # 짝지어 제거하기
 # 스택
 def solution(s):
-    words = [] # 임시 보관함
+    words = []
 
-    if len(s) % 2 == 1: # 홀수면 무조건 0
+    for word in s:
+        if words and words[-1] == word:
+            words.pop()
+        else:
+            words.append(word)
+
+    if words:
         return 0
     else:
-        for word in s:
-            if words and words[-1] == word:
-                words.pop()
-            else:
-                words.append(word)
-
-        if words:
-            return 0
-        else:
-            return 1
+        return 1
