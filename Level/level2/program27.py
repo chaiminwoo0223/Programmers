0af735caa425d4760c363f9ab20fae0585e613bb
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-11-13 18:29:45 +0900

    뒤에 있는 큰 수 찾기

--- Level/level2/program27.py
+++ Level/level2/program27.py
@@ -1,26 +1,15 @@
-# 네트워크
-# bfs
-from collections import deque
+# 뒤에 있는 큰 수 찾기
+# 스택
+# 배열 순회를 하지 않고 인덱스를 이용한 탐색은 시간을 단축시킨다.
+def solution(numbers):
+    stack = []  # 인덱스 스택
+    result = [-1] * len(numbers)
 
-def solution(n, computers):
-    answer = 0
-    visited = [0] * n
+    for i, number in enumerate(numbers):
+        while stack and numbers[stack[-1]] < number:
+            idx = stack.pop()
+            result[idx] = number
 
-    for i in range(n):
-        if visited[i] == 0:
-            bfs(n, computers, i, visited)
-            answer += 1
+        stack.append(i)
 
-    return answer
-
-def bfs(n, computers, i, visited):
-    visited[i] = 1
-    queue = deque([i])
-
-    while queue:
-        i = queue.popleft()
-        visited[i] = 1
-
-        for j in range(n):
-            if not visited[j] and computers[i][j]:
-                queue.append(j)
+    return result
