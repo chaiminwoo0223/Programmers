b9ed5cc8bf0b4651c532b03899fa09b5288bd60b
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-11-23 01:22:34 +0900

    미로 탈출

--- Level/level2/program28.py
+++ Level/level2/program28.py
@@ -7,22 +7,19 @@ def solution(maps):
     count1 = bfs('S', 'L', maps)
     count2 = bfs('L', 'E', maps)
 
-    if count1 != -1 and count2 != -1:
+    if count1 != 0 and count2 != 0:
         return count1 + count2
     else:
         return -1
 
-
 def bfs(start, end, maps):
     queue = deque()
+    visited = [[0] * len(maps[0]) for _ in range(len(maps))]
     dy = [1, 0, -1, 0]
     dx = [0, 1, 0, -1]
-    n = len(maps)
-    m = len(maps[0])
-    visited = [[0] * m for _ in range(n)]
 
-    for i in range(n):
-        for j in range(m):
+    for i in range(len(maps)):  # y
+        for j in range(len(maps[0])):  # x
             if maps[i][j] == start:
                 queue.append([i, j, 0])
                 visited[i][j] = 1
@@ -41,8 +38,8 @@ def bfs(start, end, maps):
             ty = y + dy[i]
             tx = x + dx[i]
 
-            if 0 <= ty < n and 0 <= tx < m and maps[ty][tx] != 'X' and not visited[ty][tx]:
+            if 0 <= ty < len(maps) and 0 <= tx < len(maps[0]) and maps[ty][tx] != 'X' and not visited[ty][tx]:
                 queue.append([ty, tx, count + 1])
                 visited[ty][tx] = 1
 
-    return -1
+    return 0
