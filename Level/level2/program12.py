82aa91e29fed27b371a4bd1e42a3583bf48d56ff
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-11-23 23:38:36 +0900

    H-Index

--- Level/level2/program12.py
+++ Level/level2/program12.py
@@ -1,10 +1,10 @@
 # H-Index
+# sorted(citations, reverse=True)를 통해 나머지 논문이 h번 이하 인용 처리
 def solution(citations):
-    citations = sorted(citations, reverse=True)  # 내림차순 정렬
-    paper_count = len(citations)
+    h_index = 0
 
-    for h_index in range(paper_count, -1, -1):
-        if h_index <= sum([1 for citation in citations if citation >= h_index]):
-            return h_index
+    for citation in sorted(citations, reverse=True):
+        if citation > h_index:
+            h_index += 1
 
-    return 0
+    return h_index
