725222ce13e25d1465be62a92fdd6e1bf9be7b8a
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-11-23 22:41:23 +0900

    게임 맵 최단거리

--- Level/level2/program22.py
+++ Level/level2/program22.py
@@ -4,25 +4,28 @@
 from collections import deque
 
 def solution(maps):
-    q = deque([[0, 0]])
+    return bfs(maps)
+
+def bfs(maps):
+    queue = deque([[0, 0, 1]])
+    dy = [-1, 0, 1, 0]
+    dx = [0, 1, 0, -1]
     n = len(maps)
     m = len(maps[0])
+    visited = [[0] * m for _ in range(n)]
 
-    dx = [0, -1, 0, 1]
-    dy = [-1, 0, 1, 0]
+    while queue:
+        y, x, count = queue.popleft()
 
-    while q:
-        x, y = q.popleft()
+        if y == (n - 1) and x == (m - 1):
+            return count
 
         for i in range(4):
-            tx = x + dx[i]
             ty = y + dy[i]
+            tx = x + dx[i]
 
-            if 0 <= tx < n and 0 <= ty < m and maps[tx][ty] == 1:
-                maps[tx][ty] += maps[x][y]
-                q.append([tx, ty])
-
-        if x == n - 1 and y == m - 1 and maps[x][y] > 1:
-            return maps[x][y]
+            if 0 <= ty < n and 0 <= tx < m and maps[ty][tx] != 0 and not visited[ty][tx]:
+                queue.append([ty, tx, count + 1])
+                visited[ty][tx] = 1
 
     return -1
