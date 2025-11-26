bfdb1e4eb463c0ec75c536a78581d164fe992769
Choi, Minwoo <chaiminwoo0223@gmail.com>
2025-11-26 22:18:42 +0900

    로또의 최고 순위와 최저 순위

--- Level/level1/program61.py
+++ Level/level1/program61.py
@@ -1,18 +1,8 @@
 # 로또의 최고 순위와 최저 순위
 # 조건을 정확히 반영해야 문제가 풀린다.
-from collections import Counter
-
 def solution(lottos, win_nums):
-    lottos = Counter(lottos)
-    win_nums = Counter(win_nums)
+    ranking = [6, 6, 5, 4, 3, 2, 1]
+    zero = lottos.count(0)
+    hit = sum(1 for win_num in win_nums if win_num in lottos)
 
-    count1 = len(lottos) - len(lottos - win_nums)
-    count2 = count1 + (lottos - win_nums)[0]
-
-    return [calculator(count2), calculator(count1)]
-
-def calculator(x):
-    if x < 2:
-        return 6
-    else:
-        return 7 - x
+    return [ranking[zero + hit], ranking[hit]]
