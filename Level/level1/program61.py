9624e0fa3723c6b49f35ea6639655fad92b10c2c
Choi, Minwoo <chaiminwoo0223@gmail.com>
2026-07-22 21:39:28 +0900

    로또의 최고 순위와 최저 순위

--- Level/level1/program61.py
+++ Level/level1/program61.py
@@ -1,9 +1,9 @@
 # 로또의 최고 순위와 최저 순위
 # 시뮬레이션
 def solution(lottos, win_nums):
-    rank = [6, 6, 5, 4, 3, 2, 1]
+    result = [6, 6, 5, 4, 3, 2, 1]
 
     zeros = lottos.count(0)
     matched = len(set(lottos) & set(win_nums))
 
-    return [rank[matched + zeros], rank[matched]]
+    return [result[matched + zeros], result[matched]]
